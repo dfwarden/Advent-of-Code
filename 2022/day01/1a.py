6df81387a92a2e88c500bdfad1bc5b5fabbf2c5a
David Warden <dfwarden@gmail.com>
2022-12-04 12:56:08 -0500

    finished day 1

---
+++ 2022/day01/1a.py
@@ -0,0 +1,13 @@
+elves = [list()]
+per_elf_total = [0]
+elf_idx = 0
+with open('input', 'r') as f:
+    for line in f:
+        if line == '\n':
+            elf_idx += 1
+            elves.append(list())
+            per_elf_total.append(0)
+        else:
+            item = int(line)
+            elves[elf_idx].append(item)
+            per_elf_total[elf_idx] += item
